d3e7efe21febaa6d44039511f25f9fe60913c9e5
HappyFarmer <happyfarmergo@gmail.com>
2016-07-16 19:22:19 +0800

    DataModels

---
+++ WebAPIs/WebAPIs/Models/DataModels/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIs.Models.DataModels
+{
+    public class Patient
+    {
+        public string patient_id { get; set; }
+        public string credit_num { get; set; }
+        public string password { get; set; }
+    }
+
+
+
+}
