88955da0522a3703853d1e285a08489d91b1a451
Novemser <2072806652@qq.com>
2016-07-16 16:06:25 +0800

    Data model added

--- WebAPIs/WebAPIs/Controllers/AccountController.cs
+++ WebAPIs/WebAPIs/Controllers/AccountController.cs
@@ -12,31 +12,9 @@ using WebAPIs.Models;
 
 namespace WebAPIs.Controllers
 {
-    public class AccountController : ApiController
+    public class AccountController : BaseController
     {
-        private bool GenerateUserInfoByCookie()
-        {
-            FormsAuthenticationTicket ticket = null;
-
-            var cookies = Request.Headers.GetCookies();
-            foreach (var perCookie in cookies[0].Cookies)
-            {
-                if (perCookie.Name == FormsAuthentication.FormsCookieName)
-                {
-                    ticket = FormsAuthentication.Decrypt(perCookie.Value);
-                    break;
-                }
-            }
-            if (ticket == null)
-                return false;
-
-            string[] roles = ticket.UserData.Split(',');
-            IIdentity identity = new FormsIdentity(ticket);
-            IPrincipal principal = new GenericPrincipal(identity, roles);
-            HttpContext.Current.User = principal;
-
-            return true;
-        }
+        
         [HttpPost]
         public HttpResponseMessage SignIn([FromBody]LoginUser user)
         {
--- WebAPIs/WebAPIs/Controllers/PatientController.cs
+++ WebAPIs/WebAPIs/Controllers/PatientController.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 using WebAPIs.Models;
 
 namespace WebAPIs.Controllers
 {
-    public class PatientController : ApiController
+    public class PatientController : BaseController
     {
+        public string accessRoles = "Patient";
+
         public IEnumerable<Clinic> GetAllClinic()
         {
-            
+            GenerateUserInfoByCookie();
+            // 如果用户权限正确
+            if (HttpContext.Current.User.IsInRole(accessRoles))
+            {
+                return new Clinic[1];
+            }
+            else
+            {
+                return null;
+            }
         }
+        
     }
 }
