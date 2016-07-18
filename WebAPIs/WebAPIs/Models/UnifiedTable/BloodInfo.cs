5c9d8ea9916bc6db1881359797c376c12e4a0b1f
flyboss <915741005@qq.com>
2016-07-18 20:22:31 +0800

    first test xxxInfo and xxxhelper

---
+++ WebAPIs/WebAPIs/Models/UnifiedTable/BloodInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIs.Models.UnifiedTable
+{
+    public class BloodInfo
+    {
+        const string head = "1234567890";
+        static int num = 1;
+        public string exam_id;
+        public double wbc;
+        public double neut_percent;
+        public double lymph_percent;
+        public double mono_percent;
+        public double eo_percent;
+        public double baso_percent;
+        public double neut_num;
+        public double lymph_num;
+        public double mono_num;
+        public double eo_num;
+        public double baso_num;
+        public double rbc;
+        public double hgb;
+        public double hct;
+        public double mcv;
+        public double mch;
+        public double mchc;
+        public double rdw_cv;
+        public double rdw_sd;
+        public double plt;
+        public double mpv;
+        public double pdw;
+        public double pct;
+
+        //后面还有好多项，之后确定无误再写~~~
+        public BloodInfo(double wbc)
+        {
+            this.exam_id = head + num.ToString();
+            num++;
+            this.wbc = wbc;
+        }
+    }
+}
---
+++ WebAPIs/WebAPIs/Models/UnifiedTable/ExaminationInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIs.Models.UnifiedTable
+{
+    public class ExaminationInfo
+    {
+        public string exam_id { get; set; }
+        public string type { get; set; }
+        public string exam_time { get; set; }
+        public string doc_id { get; set; }
+        public double pay { get; set; }
+        public DateTime pay_time { get; set; }
+
+        public ExaminationInfo(string exam_id,string type,string exam_time,string doc_id,double pay,DateTime pay_time)
+        {
+            this.exam_id = exam_id;
+            this.type = type;
+            this.exam_time = exam_time;
+            this.doc_id = doc_id;
+            this.pay = pay;
+            this.pay_time = pay_time;
+        }
+    }
+}
