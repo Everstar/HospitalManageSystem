2be97d226a7a7274aae40f1a71f8fc7b57ab3a7e
Novemser <2072806652@qq.com>
2016-07-19 09:26:25 +0800

    right ) added

--- WebAPIs/WebAPIs/Controllers/AccountController.cs
+++ WebAPIs/WebAPIs/Controllers/AccountController.cs
@@ -65,7 +65,6 @@ namespace WebAPIs.Controllers
         public HttpResponseMessage GetUserInfo()
         {
             string userAccount = HttpContext.Current.User.Identity.Name;
-            userAccount = "14001";
             HttpResponseMessage response = new HttpResponseMessage();
             response.Content = new StringContent(JsonObjectConverter.ObjectToJson(new Patient()));
             // 如果用户是病人
